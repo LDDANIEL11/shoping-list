0dffa29a018ca81c9f6892292cfd1c49d32bcaa5
LDDANIEL11 <lddaniel8313@gmail.com>
2025-08-13 03:25:18 +0330

    i am to tired to think , iam storing links to site i wanna read tomorrow on this file ...

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
 "use strict";
 
 document.querySelector(".btn").addEventListener("click", shoppingCart());
+
+//https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/DOM_scripting#active_learning_basic_dom_manipulation
+
+//https://eloquentjavascript.net/15_event.html
